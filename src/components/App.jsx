e9f36234981bc9ba3fdc43896413769441e7c807
nicksolony <nicksolony@gmail.com>
2023-09-15 19:09:25 -0400

    added App.styled.jsx and installed @emotion/styled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import { Container } from "./App.styled";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <Container>
       React homework template
-    </div>
+    </Container>
   );
 };
